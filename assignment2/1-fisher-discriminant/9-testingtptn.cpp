e79acd3acc119841a3e1aff3423527d752173e2c
anup-jogdand <anup.jogdand584@gmail.com>
2018-07-09 01:37:45 +0530

    Added Comments
    // Program to calculate precision,recall and accuracy

--- assignment2/1-fisher-discriminant/9-testingtptn.cpp
+++ assignment2/1-fisher-discriminant/9-testingtptn.cpp
@@ -14,7 +14,7 @@ using namespace Eigen;
 #define rev(A) reverse((A).begin(), (A).end())
 #define sorv(A) sort((A).begin(), (A).end())
 
-
+// Function to formulate gaurssian probability distribution
 vector<float> gaussian(float mu,float sigmasq,vector<float> test1d)
 {
 	for (int i = 0; i < test1d.size(); ++i)
@@ -31,7 +31,7 @@ vector<float> gaussian(float mu,float sigmasq,vector<float> test1d)
 	return test1d;
 }
 
-
+// Program to calculate precision,recall and accuracy
 int32_t main()
 {
     IOS;
@@ -40,6 +40,7 @@ int32_t main()
     float mu1 = -0.917773065212;
     float sigma1 = 0.073896710032;
     vector<float> v;
+    // Pushing predicted values
     for (int i = 0; i < 11895; ++i)
     {
     	float temp;
@@ -47,6 +48,7 @@ int32_t main()
     	v.push_back(temp);
     }
     vector<int> trueval;
+    // Pushing true values
     for (int i = 0; i < 11895; ++i)
     {
     	int temp;
@@ -58,11 +60,16 @@ int32_t main()
     vector<float> ans0 =  gaussian(mu0,sigma0,v);
     vector<float> ans1 = gaussian(mu1,sigma1,v);
     vector<float> final(11895,0);
+    // Calculating difference in gaussian outputs
     for (int i = 0; i < 11895; ++i)
     {
     	final[i] = ans1[i]-ans0[i];
     }
     float tp=0,tn=0,fp=0,fn=0;
+    // tp -> true and positive
+    // fp -> false and positive
+    // fn -> false and negative
+    // tn -> true and negative
     for (int i = 0; i < 11895; ++i)
     {
     	if (final[i]>0  && trueval[i] ==1)
@@ -75,9 +82,9 @@ int32_t main()
     		fn++;
     }
     cout<<tp<<" "<<tn<<" "<<fn<<" "<<fp<<endl;
-    float precision = tp / (tp + fp);
-    float recall = tp / ( tp + fn);
-    float accuracy = ( tp + tn ) / ( tp+ tn + fp + fn);
+    float precision = tp / (tp + fp);				// Calculating precision
+    float recall = tp / ( tp + fn);				// Calculating recall
+    float accuracy = ( tp + tn ) / ( tp+ tn + fp + fn);		// Calculating accuracy
     cout<<"precision "<<precision<<endl;
     cout<<"recall "<<recall<<endl;
     cout<<"accuracy % "<<accuracy*100<<endl;
